30a2a3e6d0b601f82333b74fdf71536eb7a58d55
Dũng Bùi <dungpro96bn@gmail.com>
2023-11-20 11:06:18 +0700

    style privacy policy page

--- wp-content/themes/tagger-travel/page.php
+++ wp-content/themes/tagger-travel/page.php
@@ -1,13 +1,5 @@
 <?php get_header(); ?>
 
-<?php /* breadcrumb */
-  global $homeName;
-  $homeName = '';
-  if (function_exists('breadcrumb')) :
-    breadcrumb();
-  endif;
-?>
-
 <?php
   if (have_posts()) : while (have_posts()) : the_post();
     remove_filter('the_content', 'wpautop');
